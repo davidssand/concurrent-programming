67b5f84fd9578d4ba237a39a7db56d1a7cf3e41a
David Sand <davidstsand@gmail.com>
2021-05-11 13:57:11 -0300

    Allow only one boat at a time in the chanel

--- T3/suez.c
+++ T3/suez.c
@@ -4,51 +4,98 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <stdarg.h>
 
+pthread_mutex_t mutex_print = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_chanel = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond_fuel = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex_map = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_chanel = PTHREAD_COND_INITIALIZER;
 
 const int map_length = 40;
 int map[map_length];
-int chanel[2] = {20, 35};
+int chanel[2] = {10, 13};
+bool chanel_in_use = false;
 
 struct boat {
     int pos_x;
+    bool after_chanel;
 };
 
-const int n_boats = 3;
+const int n_boats = 7;
 pthread_t boats_th[n_boats];
 pthread_t print_th;
 struct boat boats[n_boats];
 
-void print_map(){
-    printf("> ");
-    for (int i = 0; i < map_length; i++) {
-        if ((i >= chanel[0]) && (i <= chanel[1])) printf("\033[0;31m"); 
-        printf("%d ", map[i]);
-        if ((i > chanel[0]) && (i <= chanel[1])) printf("\033[0m");
-
-    }
-    printf(">\n\n");
+void safe_printf(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vprintf(fmt, args);
+    va_end(args);
 }
 
-void update_map(int old_pos_x, int new_pos_x) {
-    pthread_mutex_lock(&mutex_chanel);
-    map[old_pos_x] = 0;
+bool after_chanel_entrance(i) {
+    return (i >= chanel[0]);
+}
+
+bool in_chanel(i) {
+    return (after_chanel_entrance(i) && (i <= chanel[1]));
+}
+
+void print_map(){
+    safe_printf("> ");
+    for (int i = 0; i < map_length; i++) {
+        if (in_chanel(i)) safe_printf("\033[0;31m"); 
+        safe_printf("%d ", map[i]);
+        if (in_chanel(i)) safe_printf("\033[0m");
+
+    }
+    safe_printf(">\n\n");
+}
+
+void update_map(int current_pos_x, int new_pos_x) {
+    pthread_mutex_lock(&mutex_map);
+    map[current_pos_x] = 0;
     map[new_pos_x] = 1;
-    pthread_mutex_unlock(&mutex_chanel);
+    pthread_mutex_unlock(&mutex_map);
+}
+
+void update_chanel_in_use(struct boat *boat, int new_pos_x) {
+    if (!boat->after_chanel){
+        bool old_chanel_in_use = chanel_in_use;
+        chanel_in_use = in_chanel(new_pos_x);
+        if (old_chanel_in_use && !chanel_in_use) {
+            pthread_cond_broadcast(&cond_chanel);
+            boat->after_chanel = true;
+        }
+        // safe_printf("Old Chanel in use? %s\n", old_chanel_in_use ? "true" : "false");
+        // safe_printf("Chanel in use? %s\n", chanel_in_use ? "true" : "false");
+    }
+}
+
+bool nothing_at(int new_pos_x) {
+    return !map[new_pos_x];
 }
 
 void *move_boat(struct boat *boat) {
-    // printf("-------------");
     while (1) {
-        int old_pos_x = boat->pos_x;
-        int new_pos_x = old_pos_x + 1;
-        bool can_move = !map[new_pos_x];
-        if (can_move){
-            boat->pos_x = boat->pos_x + 1;
-            update_map(old_pos_x, boat->pos_x);
+        // safe_printf("-----");
+        int current_pos_x = boat->pos_x;
+        int new_pos_x = current_pos_x + 1;
+        // safe_printf("Current pos %d\n", current_pos_x);
+        // safe_printf("%d\n", new_pos_x);
+
+        pthread_mutex_lock(&mutex_chanel);
+        if (nothing_at(new_pos_x)){
+            bool boat_after_chanel_entrance = after_chanel_entrance(new_pos_x);
+            if (boat_after_chanel_entrance) update_chanel_in_use(boat, new_pos_x);
+
+            while (chanel_in_use && !boat_after_chanel_entrance) {
+                pthread_cond_wait(&cond_chanel, &mutex_chanel);
+            }
+            update_map(current_pos_x, new_pos_x);
+            boat->pos_x = new_pos_x;
         }
+        pthread_mutex_unlock(&mutex_chanel);
 		usleep(500000);
     }
 }
@@ -66,13 +113,15 @@ void initialize_map() {
         if ((i - 1) >= 0) boats[i].pos_x = boats[i - 1].pos_x + 1;
         else boats[i].pos_x = 0;
         map[boats[i].pos_x] = 1;
+        boats[i].after_chanel = false;
     }
 }
 
 void *print_thread_function(void *arg) {
 	while(1) {
+        safe_printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         print_map();
-		usleep(500000);
+		usleep(250000);
     }
 }
 
